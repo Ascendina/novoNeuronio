18dcb410c745fe9cdd1fa25dec9ca688fe3313bf
Ascendina <camilaascendina@gmail.com>
2017-08-29 11:38:23 -0300

    Adicionando as funções: adição, adição dual, ínfimo e supremo
    Adicionada as funções de adição, adição dual, ínfimo e supremo.
    Foram feitas as seguintes definições:
    - Infinitos definidos como:
    infinito positivo = double_max
    infinito negatio = -double_max
    - Supremo e ínfimo aplicado somente a números (garantia de que o ínfimo e o supremo podem ser trocados pelas funções de min e max respectivamente).

--- novoNeuronio/main.c
+++ novoNeuronio/main.c
@@ -1,8 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+
+#define infinitoPositivo DBL_MAX
+#define infinitoNegativo -DBL_MAX
+
+//* Funcao que recebe dois doubles e retorna a adicao da algebra minmax*//
+double adicao (double a, double b){
+    double resultado;
+
+    if((a == infinitoPositivo || a == infinitoNegativo) && (b == infinitoNegativo || b == infinitoPositivo) ){
+            resultado = infinitoPositivo;
+    }
+    else{
+        resultado = a + b;
+    }
+
+    return resultado;
+}
+
+//* Funcao que recebe dois doubles e retorna a adicao dual da algebra minmax*//
+double adicaoDual (double a, double b){
+   double resultado;
+
+    if((a == infinitoPositivo || a == infinitoNegativo) && (b == infinitoNegativo || b == infinitoPositivo) ){
+            resultado = infinitoNegativo;
+    }
+    else{
+        resultado = a + b;
+    }
+
+    return resultado;
+}
+
+/*Funcao que recebe dois doubles e retorna o ínfimo da morfologia matematica*/
+double infimo(double a, double b){
+    double resultado;
+
+    if(a < b){
+        resultado = a;
+    }
+    else{
+        resultado = b;
+    }
+    return resultado;
+}
+
+/*Funcao que recebe dois doubles e retorna o supremo da morfologia matematica*/
+double supremo(double a, double b){
+    double resultado;
+
+    if(a > b){
+        resultado = a;
+    }
+    else{
+        resultado = b;
+    }
+    return resultado;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    double a = 3.14;
+    double b = 4.5;
+
+    printf("adicao = %lf\n", adicao(a,b));
+    printf("adicaoDual = %lf\n", adicaoDual(a,b));
+    printf("infimo = %lf\n", infimo(a,b));
+    printf("supremo = %lf\n", supremo(a,b));
+
+    a = infinitoPositivo;
+
+    printf("adicao a infinito = %lf\n", adicao(a,b));
+    printf("adicaoDual a infinito = %lf\n", adicaoDual(a,b));
+    printf("infimo a infinito = %lf\n", infimo(a,b));
+    printf("supremo a infinito = %lf\n", supremo(a,b));
+
+
+    a = 3.14;
+    b = infinitoPositivo;
+
+    printf("adicao b infinito = %lf\n", adicao(a,b));
+    printf("adicaoDual b infinito = %lf\n", adicaoDual(a,b));
+    printf("infimo = %lf\n", infimo(a,b));
+    printf("supremo = %lf\n", supremo(a,b));
+
+
+    a = infinitoNegativo;
+    b = infinitoPositivo;
+
+    printf("adicao a e b infinito = %lf\n", adicao(a,b));
+    printf("adicaoDual a e b infinito = %lf\n", adicaoDual(a,b));
+    printf("infimo = %lf\n", infimo(a,b));
+    printf("supremo = %lf\n", supremo(a,b));
+
+    /*printf("testando valores\n");
+    printf("The minimum value of LONG = %ld\n", LONG_MIN);
+    printf("The maximum value of LONG = %ld\n", LONG_MAX);
+    printf("The minimum value of DOUBLE LONG = %.9Lf\n", -DBL_MAX);
+    printf("The maximum value of DOUBLE LONG = %.9Lf\n", DBL_MAX);*/
+
     return 0;
 }
